bcf8c9eadae23985f9ae7bf81c48e1ca5204ddd4
Amulya Shrivastava <149600311+Always-Amulya7@users.noreply.github.com>
2024-09-05 18:22:38 +0530

    JavaScript Was Updated

---
+++ WebDev Projects/NetFlix Clone ProtoType/Script-1.js
@@ -0,0 +1,19 @@
+const Button=document.getElementById("Chal")
+Button.addEventListener("click",()=>{
+    window.location.href = 'https://www.netflix.com/signup/password?locale=en-IN';
+})
+
+const Button1=document.getElementById("Chal1")
+Button1.addEventListener("click",()=>{
+    window.location.href = 'https://www.netflix.com/signup/password?locale=en-IN';
+})
+
+const Button2=document.getElementById("WorkFlow")
+Button2.addEventListener("click",()=>{
+    window.location.href = 'https://www.netflix.com/login';
+})
+
+const Button3=document.getElementById("LogoWork")
+Button3.addEventListener("click",()=>{
+    window.location.href = 'https://www.netflix.com/in/';
+})
