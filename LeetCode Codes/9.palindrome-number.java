e2e6e063e44995b8cb70ab72a7e4892157079ff8
Always-Amulya7 <amulyashrivastava7@gmail.com>
2025-08-10 17:20:20 +0530

    Project added

---
+++ LeetCode Codes/9.palindrome-number.java
@@ -0,0 +1,14 @@
+/*
+ * @lc app=leetcode id=9 lang=java
+ *
+ * [9] Palindrome Number
+ */
+
+// @lc code=start
+class Solution {
+    public boolean isPalindrome(int x) {
+        
+    }
+}
+// @lc code=end
+
